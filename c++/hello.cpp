aee0deaaecb857b73efef024a134ac90105e0962
igregorioneto <igregoriodoneto@outlook.com>
2024-02-15 19:49:21 -0300

    feat: first commit

---
+++ c++/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, world!\nBeautiful day\n";
+
+    system("pause");
+
+    return 0;
+}
---
+++ c++/variables-and-inputs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // int
+    int value;
+    value = 5;
+    cout << value << endl;
+
+    // float
+    float real;
+    real = 5.2;
+    cout << real << endl;
+
+    // double
+    double real2;
+    real2 = 5.2e99;
+    cout << real2 << endl;
+
+    // char
+    char character;
+    character = 'b';
+    cout << character << endl;
+
+    // string
+    string word;
+    word = "soccer";
+    cout << word << endl;
+
+    // exercice
+    int age;
+    cout << "Enter your age? " << endl;
+    cin >> age;
+    cout << "Age: " << age << endl;
+
+
+    return 0;
+}
